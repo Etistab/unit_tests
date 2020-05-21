a4465b4505e2306c714aa9b0546c0c875752d6c6
Germain Mathis <mathis1703@hotmail.fr>
2020-05-21 21:13:38 +0200

    Modif

--- Tu_test/src/main/java/Item.java
+++ Tu_test/src/main/java/Item.java
@@ -1,7 +1,9 @@
+import java.util.Date;
+
 public class Item {
     private String name;
     private String content;
-    private date creationDate;
+    private Date creationDate;
 
 
 
--- Tu_test/src/main/java/ToDoList.java
+++ Tu_test/src/main/java/ToDoList.java
@@ -5,4 +5,6 @@ public class ToDoList {
     private ArrayList<Item> itemList;
 
 
+
+
 }
